6c36e28dd8b125a254b82be5bc6803f3fcfa6275
Zelalem <mekonnen.zelalem251@gmail.com>
2022-08-01 20:23:55 +0300

    Fix typo on preparation_time column name

--- db/migrate/20220801152623_create_recipes.rb
+++ db/migrate/20220801152623_create_recipes.rb
@@ -2,7 +2,7 @@ class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
       t.string :name, null: false
-      t.float :prepartion_time, null: false, default: 0.0
+      t.float :preparation_time, null: false, default: 0.0
       t.float :cooking_time, null: false, default: 0.0
       t.string :description, null: false
       t.boolean :public, null: false, default: false
