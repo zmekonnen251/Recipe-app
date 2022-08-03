769a1cef8feca2b4cd12d2f21255eadbfa6d3e42
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-08-03 14:53:16 +0500

    add public_recipe controller, view and update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope  :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   # root to: "home#index"
+  
+  root "public_recipes#index"
 
   resources :foods, only: [:index, :create, :new, :destroy]
+  resources :public_recipes, only: [:index]
 end
