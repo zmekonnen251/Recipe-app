5e31c67145fe8bd68b3b1112abcfccf67c44603d
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-08-03 14:55:16 +0500

    add view index

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -3,6 +3,6 @@ class PublicRecipesController < ApplicationController
   # before_action :authenticate_user!
 
   def index
-    @public_recipes = Recipe.where(public: true).order(created_at: :desc) 
-  end  
+    @public_recipes = Recipe.where(public: true).order(created_at: :desc)
+  end
 end
